ad0581e276c0c049c9a263a8681bd9913874ff93
adean <adean@pop-os.localdomain>
2022-06-12 23:25:20 +0100

    Bookish

---
+++ bookish/redact.js
@@ -0,0 +1,73 @@
+// var log4js = require("log4js");
+// var logger = log4js.getLogger();
+// logger.level = "debug";
+
+// recalculates global redactedArray based on original text and guesses
+// also recalculates hits for the highlighted word
+var seed = 1;
+function redactText() {
+    seed = 1;
+    redactedArray.length = 0    // truncate array
+    hits = 0;
+    //debug("Guessed [" + guessedWords + "]");
+    //debug("Reserved [" + reservedWords + "]");
+    textArr.forEach(element => {
+        redactedArray.push(redactWord(element));
+    });
+    //debug("RedactedArray [" + redactedArray.join('') + "]");
+    //debug("Hits: " + hits)
+}
+
+// highlight is current guess
+function redactWord(word) {
+    if (word.match("^" + PUNC_RE + "$")) {
+        // punctuation is never redacted
+        return word;
+    }
+    var lower = word.toLowerCase();
+    //debug("Word [" + lower + "]")
+    if (reservedWords.includes(lower)) {
+        //debug("Reserved [" + lower + "]")
+        return word;
+    }
+    if (guessedWords.includes(lower)) {
+        //debug("Guessed [" + lower + "][" + highlight + "]")
+        if (lower == highlight) {
+            hits++;
+            // name the highlights (0 - indexed)
+            return "<b id='hi" + (hits - 1) + "'>" + word + "</b>";
+        } else {
+            return word;
+        }
+    }
+    if (reveal) {
+        return word;
+    }
+    // return '████████████████████'.substr(0, word.length);        // 25% too large
+    var len = word.length;
+    if (level == "hard") {
+        // uncountable image
+        return "<img src='black.jpg' style='width:" + (len * .7) + "em;'/>";
+    }
+    if (level == "medium") {
+        // random factor
+        len += random();
+    }
+    return '12345678901234567890'.substr(0, len).fixed();
+}
+
+// -1, 0, +1
+function random() {
+    var x = Math.sin(seed++) * 10000;
+    rnd = x - Math.floor(x);
+    rnd = Math.floor(rnd * 3) - 1;
+    // alert("Rnd: " + rnd);
+    return rnd;
+}
+
+function debug(str) {
+    //logger.debug(str);
+    alert(str);
+}
+
+//module.exports = redact;
---
+++ bookish/split.js
@@ -0,0 +1,28 @@
+// var log4js = require("log4js");
+// var logger = log4js.getLogger();
+// logger.level = "debug";
+
+var TAG_RE = "<[^>]*>";
+var ENT_RE = "&[^;]*;";
+var WORD_RE = "[A-Za-z0-9']{1,}";
+var PUNC_RE = "[ !(),\-\.:;/?‘’“”—]{1,}";
+
+function split(text) {
+    var out = [];
+    var i = 0;
+    // regexp should match:
+    // <tags> and </tags>
+    // &name; so i can leave mdashes etc in the samples
+    // Hello World and Alice's (straight apostrophe)
+    // groups of punctuation, including smart quotes
+    re = new RegExp(TAG_RE + "|" + ENT_RE + "|" + WORD_RE + "|" + PUNC_RE, "g");
+    while (result = re.exec(text)) {
+        out.push(result[0])
+    }
+    //alert("OUT [" + out + "]")
+    //logger.debug("Out [" + out + "]");
+
+    return out
+}
+
+//module.exports = split;
